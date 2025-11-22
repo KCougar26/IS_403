24e26035626c5b6f903ae23f4a68f3c2e784637b
mcme66 <allenschultz05@gmail.com>
2025-11-22 16:48:32 -0700

    Adding ssl configuration to database connection.

--- Project3/index.js
+++ Project3/index.js
@@ -9,7 +9,8 @@ const knex = require("knex")({
     user : process.env.RDS_USER || "bigal66",
     password : process.env.RDS_PASSWORD || "killme66",
     database : process.env.RDS_NAME || "ballroom",
-    port : process.env.RDS_PORT || 5432
+    port : process.env.RDS_PORT || 5432,
+    ssl: process.env.DB_SSL ? {rejectUnauthorized: false} : false
   }
 });
 
