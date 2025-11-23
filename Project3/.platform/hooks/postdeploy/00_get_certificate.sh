a48550e6c2da4ce574ee7b44ccd94cda03dd8b17
mcme66 <allenschultz05@gmail.com>
2025-11-22 18:34:51 -0700

    trying the actual address

--- Project3/.platform/hooks/postdeploy/00_get_certificate.sh
+++ Project3/.platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # .platform/hooks/postdeploy/00_get_certificate.sh
-sudo certbot -n -d project-allenrs.is404.net --nginx --agree-tos --email allenschultz05@gmail.com
+sudo certbot -n -d section1group12.us-east-1.elasticbeanstalk.com  --nginx --agree-tos --email allenschultz05@gmail.com
